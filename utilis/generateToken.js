965a331230e4cf49b6e2a587a73e2bb0fbecb488
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-04-07 21:59:50 -0700

    add changes

--- utilis/generateToken.js
+++ utilis/generateToken.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
+  console.log( process.env.TOKEN_KEY,process.env.TOKEN_EXPIRES )
   return jwt.sign({ id }, process.env.TOKEN_KEY, {
     expiresIn: process.env.TOKEN_EXPIRES,
   });
